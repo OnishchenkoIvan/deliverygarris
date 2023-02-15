62a096bf1005215c0701780dd37d3c918d29d265
OnishchenkoIvan <iv.onishchenko@mail.ru>
2023-02-15 20:54:24 +0300

    add: verstka

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-rational-order",
+    "stylelint-prettier/recommended",
+    "stylelint-config-prettier",
+  ],
+  plugins: ["stylelint-order", "stylelint-scss"],
+  rules: {},
+};
