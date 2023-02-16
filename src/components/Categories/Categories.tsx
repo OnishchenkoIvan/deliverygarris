ae0c5dc5b435eef7dc2532e82f1b18083b30e9da
OnishchenkoIvan <iv.onishchenko@mail.ru>
2023-02-16 21:47:26 +0300

    fix: components

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 
 export const Categories = () => {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((cat, index) => {
+          return (
+            <li
+              key={index}
+              onClick={() => setActiveIndex(index)}
+              className={activeIndex === index ? "active" : ""}
+            >
+              {cat}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
